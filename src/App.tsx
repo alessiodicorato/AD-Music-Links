8cd5a554fd2fc6015baed53ebee9d65b26d801d8
Alessio Dicorato <alessio.dicorato@outlook.com>
2026-06-12 13:05:04 +0200

    Import Poppins font, add footer logo watermark

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import "./App.css";
+import footerWatermark from "./assets/ad-watermark.png";
 
 function App() {
-	return <div className="bg-dark bg-cover min-h-screen max-w-150 m-auto"></div>;
+	return (
+		<div className="bg-black/75 w-full">
+			<div className="bg-dark bg-cover max-w-125 min-h-screen m-auto font-poppins">
+				<header></header>
+				<main></main>
+				<footer className="flex justify-center">
+					<img src={footerWatermark} alt="Footer AD Watermark" className="w-30" />
+				</footer>
+			</div>
+		</div>
+	);
 }
 
 export default App;
